225102f7b733af9bec54a56615a71b97505b3f70
dczheng <dczheng21@outlook.com>
2019-07-25 17:53:34 +0800

    update

---
+++ download_vim_plugin.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# dczheng
+# created 2019-07-25
+#
+
+git clone git@github.com:jiangmiao/auto-pairs.git    ~/.vim/auto-pairs
+git clone git@github.com:Shougo/neocomplete.vim.git  ~/.vim/neocomplete
+git clone git@github.com:mhinz/vim-signify.git       ~/.vim/signify
+git clone git@github.com:tpope/vim-surround.git      ~/.vim/surround
+git clone git@github.com:vim-syntastic/syntastic.git ~/.vim/syntastic
+git clone git@github.com:mbbill/undotree.git         ~/.vim/undotree
+
