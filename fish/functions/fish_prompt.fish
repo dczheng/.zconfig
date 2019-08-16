f14c9a71f76acb36ecdf44551d396517ccddbed9
dczheng <dczheng@outlook.com>
2019-08-16 18:31:03 +0800

    update

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -2,7 +2,7 @@ function fish_prompt
     set_color yellow 
     set prompt_dir (pwd)
     #set prompt_dir (string split / (pwd))
-    echo -e "$prompt_dir"
+    echo -e " $prompt_dir"
     set_color red 
     echo -en " > "
     set color normal
