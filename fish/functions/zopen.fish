b5c000140c4f0bea67d764f49f71801a29e6eed1
dczheng <dczheng@outlook.com>
2019-08-27 16:52:18 +0800

    update

---
+++ fish/functions/zopen.fish
@@ -0,0 +1,24 @@
+function zopen
+    set pdf_opener  zathura
+    set txt_opener  gedit
+    set img_opener  eog 
+
+    set filetype (string split "." -- $argv[1])[2]
+    switch $filetype
+
+        case "pdf"
+            printf "Open `%s` with %s" $argv[1] $pdf_opener
+            $pdf_opener  $argv[1] &
+
+        case "txt"
+            printf "Open `%s` with %s" $argv[1] $txt_opener
+            $txt_opener  $argv[1] &
+
+        case "png" "jpg" "jpeb" "bmp"
+            printf "Open `%s` with %s" $argv[1] $img_opener
+            $img_opener  $argv[1] &
+
+        case '*'
+            printf "Can't open `%s`\n" $argv[1]
+    end
+end
