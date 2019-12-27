27a708458e0600d5f111d6d30e0cdcac388ec719
bazhaoyu <bazhaoyu@bazhaoyu-t450s.sjtu.edu.cn>
2019-12-27 15:08:02 +0800

    update

--- tmux-scripts/battery-tmux.sh
+++ tmux-scripts/battery-tmux.sh
@@ -7,11 +7,11 @@
 
 warning_icon="#[fg=red][\U26A0"
 charging_icon="#[fg=green][\U26A1"
-if [ $(hostname) == archlinux"" ]
+if [ `hostname` = archlinux"" ]
 then
-    s=$( upower -i $(upower -e | grep -E 'battery|DisplayDevice') | awk '/state:/ {print $2}')
-    p=$(upower -i $(upower -e | grep -E 'battery|DisplayDevice') | awk '/percentage:/ {print $2}')
-    pp=$(echo $p | sed 's/%//')
+    s=`upower -i $(upower -e | grep -E 'battery|DisplayDevice') | awk '/state:/ {print $2}'`
+    p=`upower -i $(upower -e | grep -E 'battery|DisplayDevice') | awk '/percentage:/ {print $2}'`
+    pp=`echo $p | sed 's/%//'`
 
     x="#[fg=green][$p]"
     if [ $pp -lt 10 ]
@@ -19,7 +19,7 @@ then
         x="$warning_icon $p]"
     fi
 
-    if [ $s == 'charging' ]
+    if [ $s = 'charging' ]
     then
         x="$charging_icon $p]"
     fi
