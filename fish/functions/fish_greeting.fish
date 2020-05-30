07b4b2fd1593debee5f1df36da8bfccb9298eaac
dczheng <dczheng21@outlook.com>
2020-05-30 15:59:29 +0800

    update

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,4 +1,5 @@
-function my_welcome
+# Defined in fish_greeting.fish @ line 1
+function fish_greeting
     if status is-login
         clear 
         set sys (hostnamectl | grep "Operating" | cut -d":" -f2 )
@@ -7,7 +8,7 @@ function my_welcome
         hostnamectl | egrep -v "(ID|Chassis)"
         echo ">>>>>>>>>>>>>>>>>>>"
         echo "Last Login:"
-        lastlog | sed -n '3,$p' | egrep -v "(Never| tty)"
+        lastlog | sed -n '3,$p' | egrep -v "(Never| tty)" | awk '{print $1" from "$3}'
         echo ">>>>>>>>>>>>>>>>>>>"
         date
     end
