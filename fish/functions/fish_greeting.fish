7233ef65d4bf93c92c1eeb099ff9bb54fcbd5abd
dczheng <dczheng21@outlook.com>
2020-05-30 16:27:58 +0800

    update

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -3,7 +3,7 @@ function fish_greeting
     if status is-login
         clear 
         set sys (hostnamectl | grep "Operating" | cut -d":" -f2 )
-        cowsay.py "$USER (*.*) Welcome to $USER's $sys (*.*)"
+        cowsay.py "$USER (*.*) Welcome to $sys (*.*)"
         echo ">>>>>>>>>>>>>>>>>>>"
         hostnamectl | egrep -v "(ID|Chassis)"
         echo ">>>>>>>>>>>>>>>>>>>"
