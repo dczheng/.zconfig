e145a598db65b57c2a08a7fc09693732c8887a58
dczheng <dczheng@outlook.com>
2020-12-10 11:58:51 +0000

    update

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -2,13 +2,13 @@
 function fish_greeting
     if status is-login
         clear 
-        set sys (hostnamectl | grep "Operating" | cut -d":" -f2 )
+        #set sys (hostnamectl | grep "Operating" | cut -d":" -f2 )
         cowsay.py "$USER (*.*) Welcome to $sys (*.*)"
-        echo ">>>>>>>>>>>>>>>>>>>"
-        hostnamectl | egrep -v "(ID|Chassis)"
-        echo ">>>>>>>>>>>>>>>>>>>"
-        echo "Last Login:"
-        lastlog | sed -n '3,$p' | egrep -v "(Never| tty)" | awk '{print $1" from "$3}'
+        #echo ">>>>>>>>>>>>>>>>>>>"
+        #hostnamectl | egrep -v "(ID|Chassis)"
+        #echo ">>>>>>>>>>>>>>>>>>>"
+        #echo "Last Login:"
+        #lastlog | sed -n '3,$p' | egrep -v "(Never| tty)" | awk '{print $1" from "$3}'
         echo ">>>>>>>>>>>>>>>>>>>"
         date
     end
