50ba1fcc99400c37ae3488960c5858797ee7a0e3
--global <dczheng21@outlook.com>
2021-01-23 20:35:17 +0800

    update

---
+++ fish/functions/set-mouse.fish
@@ -0,0 +1,21 @@
+function set-mouse
+    if test -z $argv[1]
+        echo "SYNTAX ERROR!"
+       return
+    end
+
+    set opt "libinput Accel Speed"
+    echo $opt
+    xinput set-prop $argv[1] $opt 1
+
+    set opt "libinput Middle Emulation Enabled"
+    echo $opt
+    xinput set-prop $argv[1] $opt 1
+
+    set opt "libinput Scroll Method Enabled"
+    echo $opt
+    xinput set-prop $argv[1] $opt 0 0 1
+
+    xinput set-button-map $argv[1] 1 2 3
+    xinput get-button-map $argv[1]
+end
